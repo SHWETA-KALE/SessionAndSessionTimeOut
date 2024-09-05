251ba356b0c7daba9bd87225b90ad73d3b304955
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-09-06 00:27:30 +0530

    Session Timeout and Session

---
+++ POC_Sessions/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POC_Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session["MyTimeSession"] = DateTime.Now.ToString();
+            TempData["MyData"] = DateTime.Now.ToString();
+            //ViewData["MyData"] = DateTime.Now.ToString();
+            //return View();
+            return RedirectToAction("Home");
+        }
+
+        public ActionResult Home()
+        {
+            //ViewData["MyData"] = DateTime.Now.ToString();
+            //using viewbag
+            //ViewBag.MyTime = DateTime.Now.ToString();
+            return View();
+        }
+
+        public ActionResult DemonstrationSession()
+        {
+            return View();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//viewbag is just a syntactic sugar of viewdata both are used for passing data from controller to view
+//viewbag or viewdata does not maintain the data from controller to controller or from one action method to another action method hence w use
+//temp data for that
+//temp data is only for one req from action to action or action to view
+//view bag is dyanamic it perform internal reflection
---
+++ POC_Sessions/Controllers/SessionTimeoutController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POC_Sessions.Controllers
+{
+    public class SessionTimeoutController : Controller
+    {
+        // GET: Sessiontimeout
+        public ActionResult Index()
+        {
+            //setting session data 
+            Session["UserName"] = "Shweta Kale";
+
+            //setting session timeout to 1 min for demonstration
+            Session.Timeout = 1;
+            return View();
+        }
+
+        public ActionResult CheckSession()
+        {
+            if (Session["UserName"] != null)
+            {
+                ViewBag.Message = $"Session is active. User:{Session["UserName"]}";
+            }
+            else
+            {
+                ViewBag.Message = "Session has expired.";
+            }
+
+            return View();
+        }
+    }
+}
